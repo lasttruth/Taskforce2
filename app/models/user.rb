79014af9b0e12d418a5077e238fa636f20118df4
Robin Doirin <robindoirin@gmail.com>
2019-01-27 22:19:37 +0000

    sign-up updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :games
+
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 4 }
 end

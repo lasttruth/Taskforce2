57183f706516f7334c6cb2d06c543999dc4b3163
Robin Doirin <robindoirin@gmail.com>
2019-01-27 17:59:29 +0000

    ide bug not showing sessions controller but added a login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games
   resources :users
   root 'users#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end

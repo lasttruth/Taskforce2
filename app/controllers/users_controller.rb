d35971de0fa57bf6ee58035224ce8aa35b9a9b2b
Robin Doirin <robindoirin@gmail.com>
2019-01-29 03:10:19 +0000

    sign-up fully functioning user is saved to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,13 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Task Force!!"
+       redirect_to @user
+    else
+    render 'new'
+    end
   end
 
   def update
@@ -23,4 +29,10 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  private
+  def user_params
+      params.require(:user).permit(:email, :password,
+                                   :password_confirmation)
+    end
 end

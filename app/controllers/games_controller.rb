4138000e105d84722d2ebb7ffdd3d9e6df80937a
Robin Doirin <robindoirin@gmail.com>
2019-03-28 17:52:49 +0000

    added game CRUD

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,49 @@
 class GamesController < ApplicationController
   def index
-  end
-
-  def edit
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    #render plain: params[:game].inspect
+    @game = Game.new(game_params)
+    if (@game.save)
+      redirect_to @game
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+      @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+    if (@game.update(game_params))
+      redirect_to @game
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+
+    redirect_to games_path
+  end
+
+private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :platform)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :games
-  resources :users
+  resources :users do
+    resources :games, only: [:index, :show]
+  end
   root 'users#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'

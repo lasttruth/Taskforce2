403deb9a2537d0714cfc3ee613da8dc86a8688a3
Robin Doirin <robindoirin@gmail.com>
2019-03-28 02:15:40 +0000

    completed login crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,30 @@
-class UsersController < ApplicationController
-  def index
-    @users = User.all
+def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def show
-
-  end
-
-  def new
-     @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
-      flash[:success] = "Welcome to the Task Force!!"
-       redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-      if @user.update_attributes(user_params)
-        flash[:success] = "Profile updated"
-        redirect_to @user
-      else
-        render 'edit'
-      end
-  end
-
-  def destroy
-    log_out
-    redirect_to root_url
-  end
-
   private
-  def user_params
-      params.require(:user).permit(:email, :password,
+
+    def user_params
+      params.require(:user).permit( :email, :password,
                                    :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games
@@ -10,6 +9,6 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
 
 end
